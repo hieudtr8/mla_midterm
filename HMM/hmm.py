714c7efad7bdd01f8214b0fc2edc56183b55c19e
Nguyen Manh Hung <hungnm2.bhsoft@gmail.com>
2023-11-07 19:56:19 +0700

    Remove unnecessary HMM code

--- HMM/hmm.py
+++ HMM/hmm.py
@@ -59,7 +59,7 @@ sample_space_frachp = np.linspace(frachp.min(), frachp.max(), 10)
 possible_outcomes = np.array(list(itertools.product(
     sample_space_fracocp, sample_space_frachp, sample_space_fraclp)))
 num_latent_days = 50
-num_days_to_predict = 300
+num_days_to_predict = 200
 
 predicted_close_prices = []
 for i in tqdm(range(num_days_to_predict)):
@@ -90,91 +90,5 @@ x_axis = np.array(
 plt.plot(x_axis, test_data.iloc[0:num_days_to_predict]
          ['Close'], 'b+-', label="Actual close prices")
 plt.plot(x_axis, predicted_close_prices, 'ro-', label="Predicted close prices")
-plt.legend(prop={'size': 20})
-ae = abs(test_data.iloc[0:num_days_to_predict]
-         ['Close'] - predicted_close_prices)
-
-plt.figure(figsize=(30, 10), dpi=80)
-
-plt.plot(x_axis, ae, 'go-', label="Error")
-plt.legend(prop={'size': 20})
-print("Max error observed = " + str(ae.max()))
-print("Min error observed = " + str(ae.min()))
-print("Mean error observed = " + str(ae.mean()))
-num_latent_days_values = [10, 20, 30, 40, 50, 60]
-baseline_num_latent_days = 50
-n_components_values = [4, 6, 8, 10, 12, 14]
-baseline_n_componets = 10
-num_steps_values = [10, 20, 40, 50]
-baseline_num_steps = 50
-num_days_to_predict = 100  # We don't need to predict as many days as before
-mae_num_components = []
-for num_component in n_components_values:
-    model = GaussianHMM(n_components=num_component)
-    model.fit(features_train)
-    predicted_close_prices = []
-    for i in tqdm(range(num_days_to_predict)):
-        # Calculate start and end indices
-        previous_data_start_index = max(0, i - baseline_num_latent_days)
-        previous_data_end_index = max(0, i)
-        # Acquire test data features for these days
-        previous_data = extract_features(augment_features(
-            test_data.iloc[previous_data_start_index:previous_data_end_index]))
-
-        outcome_scores = []
-        for outcome in possible_outcomes:
-            # Append each outcome one by one with replacement to see which sequence generates the highest score
-            total_data = np.row_stack((previous_data, outcome))
-            outcome_scores.append(model.score(total_data))
-
-        # Take the most probable outcome as the one with the highest score
-        most_probable_outcome = possible_outcomes[np.argmax(outcome_scores)]
-        predicted_close_prices.append(
-            test_data.iloc[i]['Open'] * (1 + most_probable_outcome[0]))
-    mae_num_components.append(
-        (abs(test_data.iloc[0:num_days_to_predict]['Close'] - predicted_close_prices)).mean())
-plt.figure(figsize=(30, 10), dpi=80)
-
-plt.plot(n_components_values, mae_num_components, 'go-', label="Error")
-plt.xlabel("Number of hidden states")
-plt.ylabel("MAE")
-plt.legend(prop={'size': 20})
-mae_num_steps = []
-model = GaussianHMM(n_components=baseline_n_componets)
-model.fit(features_train)
-for num_step in num_steps_values:
-    sample_space_fracocp = np.linspace(fracocp.min(), fracocp.max(), num_step)
-    sample_space_fraclp = np.linspace(
-        fraclp.min(), frachp.max(), int(num_step/5))
-    sample_space_frachp = np.linspace(
-        frachp.min(), frachp.max(), int(num_step/5))
-    possible_outcomes = np.array(list(itertools.product(
-        sample_space_fracocp, sample_space_frachp, sample_space_fraclp)))
-    predicted_close_prices = []
-    for i in tqdm(range(num_days_to_predict)):
-        # Calculate start and end indices
-        previous_data_start_index = max(0, i - baseline_num_latent_days)
-        previous_data_end_index = max(0, i)
-        # Acquire test data features for these days
-        previous_data = extract_features(augment_features(
-            test_data.iloc[previous_data_start_index:previous_data_end_index]))
-
-        outcome_scores = []
-        for outcome in possible_outcomes:
-            # Append each outcome one by one with replacement to see which sequence generates the highest score
-            total_data = np.row_stack((previous_data, outcome))
-            outcome_scores.append(model.score(total_data))
-
-        # Take the most probable outcome as the one with the highest score
-        most_probable_outcome = possible_outcomes[np.argmax(outcome_scores)]
-        predicted_close_prices.append(
-            test_data.iloc[i]['Open'] * (1 + most_probable_outcome[0]))
-    mae_num_steps.append(
-        (abs(test_data.iloc[0:num_days_to_predict]['Close'] - predicted_close_prices)).mean())
-plt.figure(figsize=(30, 10), dpi=80)
-
-plt.plot(num_steps_values, mae_num_steps, 'go-', label="Error")
-plt.xlabel("Number of intervals for features")
-plt.ylabel("MAE")
 plt.legend(prop={'size': 20})
 plt.show()
